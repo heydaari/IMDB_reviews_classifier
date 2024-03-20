7bdddb5b2bf5cc43577c560e94d2139b9c79ea0d
Mohammad Hassan <heidary0081@gmail.com>
2024-03-20 22:24:49 +0330

    Update bert_classifier.py

--- bert_classifier.py
+++ bert_classifier.py
@@ -9,15 +9,12 @@ imdb_train, imdb_test = tfds.load("imdb_reviews",
                                   as_supervised=True,
                                   batch_size=1)
 
-print(list(imdb_train)[0][0])
-print(list(imdb_train)[0][1])
-exit()
 
 # Load a BERT model
 classifier = keras_nlp.models.BertClassifier.from_preset("bert_base_en_uncased", num_classes=2)
-
+print(classifier.summary())
 # Fine-tune on IMDb movie reviews
-classifier.fit(imdb_train, )
+classifier.fit( imdb_train)
 
 # Predict on new examples
 classifier.predict(["What an amazing movie!", "A total waste of my time."])
