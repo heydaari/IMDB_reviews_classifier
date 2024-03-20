ea182040e1adc698832ca56590d3769caa5cf4a8
Mohammad Hassan <heidary0081@gmail.com>
2024-03-20 22:45:35 +0330

    hugging face classifier completed

--- huggingFace_transformers.py
+++ huggingFace_transformers.py
@@ -1,2 +1,10 @@
 from transformers import pipeline
+import pandas as pd
 
+classifier = pipeline(task='text-classification')
+
+review = 'what a great movie! i really want to see it multiple times again!'
+model_response = classifier(inputs=review)
+
+response = pd.DataFrame(model_response)
+print(response)
